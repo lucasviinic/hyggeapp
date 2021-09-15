c00ef575dceff22e53402bd0e047a0dbde7b097b
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-14 22:31:38 -0300

    Botões de pesquisa e limpeza

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text } from 'react-native'
 
+import Footer from '../../components/Footer'
 import SearchField from '../../components/SearchField'
+
 import Logo from '../../assets/logo.svg'
 import Lupa from '../../assets/lupa.svg'
 
 import {
   Container,
-  Footer,
   SearchArea,
+  SearchAndCleanButtons,
+  Button
 } from './styles'
 
 export default function Home({ navigation }) {
@@ -17,10 +20,16 @@ export default function Home({ navigation }) {
             <SearchArea>
                 <Logo width={200} marginLeft={"auto"} marginRight={"auto"} marginBottom={-60} />
                 <SearchField navigation={navigation} />
+                <SearchAndCleanButtons>
+                  <Button>
+                      <Text style={{fontSize: 15}}>Search</Text>
+                  </Button>
+                  <Button>
+                      <Text style={{fontSize: 15}}>Clean</Text>
+                  </Button>
+                </SearchAndCleanButtons>
             </SearchArea>
-            <Footer>
-                <Text style={{color: "#EBEBEB", fontSize: 12}}>Copyright © 2021 - Käufer</Text>
-            </Footer>
+            <Footer />
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,4 +16,44 @@ export const SearchArea = styled.View`
   margin-bottom: 110px;
 `;
 
+export const SearchAndCleanButtons = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 25px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  height: 45px;
+  width: 110px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 13px;
+  margin-left: 10px;
+  padding-top: 10px;
+  padding-right: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  align-items: center;
+`;
+
+export const SearchField = styled.View`
+  flex-direction: row;
+  align-items: center;
+  height: 43px;
+  width: 300px;
+  border-radius: 25px;
+  background-color: rgba(255, 255, 255, 0.7);
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+  padding-top: 10px;
+  padding-right: 10px;
+  padding-bottom: 10px;
+  padding-left: 17px;
+  color: #424242;
+`;
+
 
