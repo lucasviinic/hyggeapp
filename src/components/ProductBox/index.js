01ececbd380dd24e75520645a25b1f00bf80b9d6
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-15 23:07:20 -0300

    Separando o componente box em dois

--- src/components/ProductBox/index.js
+++ src/components/ProductBox/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Text} from 'react-native'
 
-import {Box, ImageField} from './styles'
+import {Box, ImageField, ProductInfoField} from './styles'
 
 export default function ProductBox(){
   return(
     <Box>
       <ImageField>
-
+        <Text style={{fontSize: 22, color: "#FFFFFF", opacity: 0.3}}>Image Field</Text>
       </ImageField>
+      <ProductInfoField>
+        <Text style={{fontSize: 22, color: "#FFFFFF", opacity: 0.3}}>Product {"\n"}Info Field</Text>
+      </ProductInfoField>
     </Box>
   )
 }
--- src/components/ProductBox/styles.js
+++ src/components/ProductBox/styles.js
@@ -1,21 +1,35 @@
 import styled from 'styled-components';
 
 export const Box = styled.View`
+  flex: 1;
   width: 345px;
   height: 120px;
   margin-top: 6px;
-  border-radius: 10px;
-  align-items: center;
+  flex-direction: row;
   background-color: rgba(51, 23, 108, 1);
+  border-radius: 10px;
 `;
 
 export const ImageField = styled.View`
   flex: 1;
-  width: 118px;
+  width: 103px;
   height: auto;
   margin-right: auto;
-  align-items: center;
+  padding-top: 25px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+  padding-left: 29px;
   border-right-width: 1.3px;
   border-style: solid;
   border-color: #5638A9;
+`;
+
+export const ProductInfoField = styled.View`
+  width: 223px;
+  height: auto;
+  margin-left: auto;
+  padding-top: 26px;
+  padding-right: auto;
+  padding-bottom: auto;
+  padding-left: 45px;
 `;
