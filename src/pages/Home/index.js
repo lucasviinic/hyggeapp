0f20b4a68eeb9b859c6ab05ce8f5a4c18835c11e
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-25 17:23:02 -0300

    Recebendo dados da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { Text } from 'react-native'
+import api from '../../services/Api';
 
 import Footer from '../../components/Footer'
 import SearchField from '../../components/SearchField'
@@ -13,6 +15,19 @@ import {
 } from './styles'
 
 export default function Home({ navigation }) {
+    const [product, setProduct] = useState(null)
+
+    async function loadProduct(){
+      api.get('/search/?s=https://www.apple.com/shop/buy-mac/macbook-air/')
+        .then(response => setProduct(response.data))
+        .catch(err => console.log(err))
+
+      return product != null ? loadProduct() : product
+    }
+
+    useEffect(() => {
+      loadProduct()
+    }, [])
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -27,6 +42,7 @@ export default function Home({ navigation }) {
 
     return(
         <Container>
+            <Text>{product ? product.name : "Carregando..."}</Text>
             <SearchArea>
                 <Logo width={200} marginLeft={"auto"} marginRight={"auto"} marginBottom={-60} />
                 <SearchField navigation={navigation} />
