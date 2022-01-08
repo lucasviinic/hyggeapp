6070e6d9d686e49cc462f44252ccc6d7e23c31d7
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2022-01-08 11:54:21 -0300

    Tratando erros no login
    Lidando com os erros de nome de usuário inexistente e senha incorreta

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -2,6 +2,7 @@ import React, { createContext, useState, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../services/api';
 import * as auth from '../services/auth';
+import { Alert } from 'react-native';
 
 const AuthContext = createContext({signed: true, user: {}, loading: true, signIn(){}});
 
@@ -28,16 +29,24 @@ export const AuthProvider = ({children}) => {
   async function signIn(data) {
     const response = await auth.signIn(data);
 
-    setUser(response.data.user);
+    if ("user" in response.data) {
+      setUser(response.data.user);
 
-    api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+      api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
 
-    await AsyncStorage.setItem('RNAuth:user', JSON.stringify(response.data.user));
-    await AsyncStorage.setItem('RNAuth:token', response.data.token);
+      await AsyncStorage.setItem('RNAuth:user', JSON.stringify(response.data.user));
+      await AsyncStorage.setItem('RNAuth:token', response.data.token);
+
+    } else {
+      Alert.alert('Erro', response.data.non_field_errors[0]);
+      console.log('Erro no login: ', response.data.non_field_errors[0]);
+    }
   }
 
   function signOut(){
+    const username = user.username;
     AsyncStorage.clear().then(() => {
+      console.log(`Usuário ${username} deslogado com sucesso!`);
       setUser(null)
     });
   }
