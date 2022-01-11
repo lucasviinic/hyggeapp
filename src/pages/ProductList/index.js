e5086c836a65420bb4c866a56374ff354cc3cbc7
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2022-01-10 21:01:10 -0300

    Só atualiza os dados quando o usuário criar ou deletar um pedido

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -11,13 +11,8 @@ export default function ProductList({ route, navigation }){
 
   const { products, loadProducts } = useApp();
 
-  //executa loadPorducts a cada 5 segundos
   useEffect(() => {
-    const interval = setInterval(() => {
-      loadProducts();
-    }, 15000);
-
-    return () => clearInterval(interval);
+    loadProducts();
   }, [])
 
   return(
